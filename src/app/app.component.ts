a26167b456718046611a3ff443b2846f7343ba48
Diego Pinto <dicapisar@gmail.com>
2021-01-27 11:24:55 -0500

    Creación de funcionalidad de Creación, Edición y Eliminación de Empleados del Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,32 @@ import { Employee } from './models/employee'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   employeedArray: Employee[] = [
     {id: 1, name: "Ryan", country: "USA"},
     {id: 2, name: "Angelica", country: "USA"},
     {id: 3, name: "Joe", country: "USA"}
   ];
 
-  
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+
+    if(this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeedArray.length + 1;
+      this.employeedArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete(){
+    if(confirm('¿Estas Seguro de querer elminar el registro?')){
+      this.employeedArray = this.employeedArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
