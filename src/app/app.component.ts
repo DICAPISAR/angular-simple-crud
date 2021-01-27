3fc8d0d38c5346636159f602b26342d95a5aed76
Diego Pinto <dicapisar@gmail.com>
2021-01-27 09:18:48 -0500

    Creación del modulo Employed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeedArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "USA"},
+    {id: 2, name: "Angelica", country: "USA"},
+    {id: 3, name: "Joe", country: "USA"}
+  ];
+
+  
 }
